4ae5672ac52e4c8e8d73b39328ef14df101795c6
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2022-03-30 14:29:52 -0700

    Updates seed data

--- seedData.js
+++ seedData.js
@@ -6,78 +6,93 @@ const seedData = {
   },
   workouts: [
     {
-      name: 'Tourmaline Surf Sesh',
-      description: 'Great beginner spot - easy to catch waves',
-      location: 'Tourmaline Surf Park'
+      name: 'Leg Day',
+      description: 'For building an impressive pair of gams.',
+      length: 120,
+      location: 'Galaxy Fitness Gym'
     },
     {
-      name: 'Marsh Workout',
-      description: 'Marsh by my house',
+      name: 'Back Workout',
+      description: 'Go slow on this one if it\'s been a while.',
       length: 120,
-      location: 'Famosa Slough'
+      location: 'Galaxy Fitness Gym'
     },
     {
-      name: 'Gym Workout',
-      description: 'pumping iron at the place down the street',
+      name: 'Shoulder Workout',
+      description: 'Studies show that shoulder workouts relieve pain and decrease the possibility of future dislocations.',
       length: 120,
-      location: 'Point Loma Gym'
-    }
+      location: 'Galaxy Fitness Gym'
+    },
+    {
+      name: 'Chest Workout',
+      description: 'Get there early while the benches are open.',
+      length: 120,
+      location: 'Galaxy Fitness Gym'
+    },
   ],
   exercises: [
     {
-      name: 'Leg stretches',
-      reps: 10,
-      workoutIndex: 0
-    },
-    {
-      name: 'Stand ups',
-      reps: 10,
-      workoutIndex: 0
-    },
-    {
-      name: 'Paddle Sprints',
-      reps: 2,
-      workoutIndex: 0
-    },
-    {
-      name: 'Push ups',
-      reps: 20,
-      sets: 3,
-      workoutIndex: 1
-    },
-    {
-      name: 'Upsidedown Shoulder Press',
-      reps: 15,
-      sets: 3,
-      workoutIndex: 1
-    },
-    {
-      name: 'Squats',
-      reps: 10,
-      sets: 3,
-      workoutIndex: 1
-    },
-    {
-      name: 'Bicep Curl',
-      reps: 10,
-      sets: 3,
-      weight: 15,
-      unit: 'lbs',
-      workoutIndex: 2
-    },
-    {
-      name: 'Chest Press',
-      reps: 8,
+      name: 'Barbell Back Squat',
+      reps: 5,
       sets: 3,
       weight: 40,
+      workoutIndex: 0
+    },
+    {
+      name: 'Romanian Deadlift',
+      reps: 6,
+      sets: 3,
+      weight: 40,
+      workoutIndex: 0
+    },
+    {
+      name: 'Walking Dumbbell Lunge',
+      reps: 6,
+      sets: 3,
+      weight: 30,
+      workoutIndex: 0
+    },
+    {
+      name: 'Deadlift',
+      reps: 5,
+      sets: 3,
+      weight: 40,
+      workoutIndex: 1
+    },
+    {
+      name: 'Bent Row',
+      reps: 10,
+      sets: 3,
+      weight: 30,
+      workoutIndex: 1
+    },
+    {
+      name: 'Pull-up',
+      reps: 5,
+      sets: 3,
+      workoutIndex: 1
+    },
+    {
+      name: 'Seated Dumbbell Shoulder Press',
+      reps: 10,
+      sets: 3,
+      weight: 30,
       unit: 'lbs',
       workoutIndex: 2
     },
     {
-      name: 'Shoulder Press',
+      name: 'Front Raise',
       reps: 8,
       sets: 3,
-      weight: 30,
+      weight: 14,
+      unit: 'lbs',
+      workoutIndex: 2
+    },
+    {
+      name: 'Dumbbell Lateral Raise',
+      reps: 8,
+      sets: 3,
+      weight: 14,
       unit: 'lbs',
       workoutIndex: 2
     }
