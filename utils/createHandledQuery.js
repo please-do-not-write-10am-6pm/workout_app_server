706c639dc7cc81638ab43da56dfa2436729d9472
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2022-04-27 14:31:59 -0700

    Removes error throwing in handledQuery

--- utils/createHandledQuery.js
+++ utils/createHandledQuery.js
@@ -1,14 +1,11 @@
 const { ApolloError } = require('apollo-server')
 
-// createHandledQuery - Creates a function that when called will try the
-// query or return an error
 function createHandledQuery(query) {
   return async function (...args) {
     try {
       return await query(...args)
     } catch (err) {
       console.log('err', err)
-      throw new ApolloError('There was an error retrieving your data.')
     }
   }
 }
