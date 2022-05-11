7917e73615bb09ca88120c70689bca93c1f36e48
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2022-05-11 14:33:11 -0700

    Allows cross-site cookie to be sent

--- src/utils/setTokenCookie.ts
+++ src/utils/setTokenCookie.ts
@@ -2,7 +2,9 @@ interface ContextType {
   res: {
     cookie: (cookieName: string, value: string, config: {
       httpOnly: boolean,
-      maxAge: number
+      maxAge: number,
+      sameSite: string,
+      secure: boolean
     }) => void
   }
 }
@@ -10,7 +12,9 @@ interface ContextType {
 function setTokenCookie(context: ContextType, token: string): void {
   context.res.cookie('token', token, {
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 // 1 day
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    sameSite: 'none',
+    secure: true
   })
 }
 
